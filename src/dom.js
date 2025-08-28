0598563914d78d715175a583e2f036051f71c337
Justin Chapdelaine <10560997+justinchapdelaine@users.noreply.github.com>
2025-08-27 18:37:14 -0700

    chore(frontend): remove JS duplicates under src and tests; TS is now the single source

--- src/dom.js
+++
@@ -1,6 +0,0 @@
-export function el(tag, props = {}, ...children) {
-  const e = document.createElement(tag)
-  Object.assign(e, props)
-  children.forEach((c) => e.append(typeof c === 'string' ? document.createTextNode(c) : c))
-  return e
-}
